349aa536f28007dc16b2d7ce7fdf730321dd1d72
Ryanns7 <ryanns777@gmail.com>
2025-03-08 12:51:18 -0300

    perf: added a condition to check the results of the function

--- backend/src/application/usecases/balance/deductValueFromTheTotalBalanceUseCase.ts
+++ backend/src/application/usecases/balance/deductValueFromTheTotalBalanceUseCase.ts
@@ -1,6 +1,7 @@
 import { IBalanceRepository } from "../../../domain/usecases/finance/balanceRepository";
+import { NotFound, NumberError } from "../../../errors/baseError";
 
-export class InsertValueIntoTotalBalance{
+export class DeductValueIntoTotalBalance{
     balanceRepo: IBalanceRepository
 
     constructor(balanceRepo: IBalanceRepository){
@@ -8,6 +9,12 @@ export class InsertValueIntoTotalBalance{
     }
 
     async deductValue(amountToBeDeducted: number, id_user: string){
+
+        const balance = await this.balanceRepo.getBalance(id_user)
+
+        if(balance instanceof NotFound){
+            return balance
+        }
 
         const deduct = await this.balanceRepo.deductValueFromTheTotalBalance(amountToBeDeducted, id_user)
 
