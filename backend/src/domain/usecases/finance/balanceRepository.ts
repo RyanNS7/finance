a52287a72ac660bf1024a01a4c0116c82957519c
Ryanns7 <ryanns777@gmail.com>
2025-03-08 12:48:38 -0300

    refactor: added the error value inside the promise in case the function has any error

--- backend/src/domain/usecases/finance/balanceRepository.ts
+++ backend/src/domain/usecases/finance/balanceRepository.ts
@@ -1,10 +1,11 @@
+import { NotFound, NumberError } from "../../../errors/baseError";
 import { amountSpent } from "../../entities/finance/amountSpent";
 import { AmountSpentDTO } from "../../entities/finance/amountSpentDTO";
 import { TotalBalanceDTO } from "../../entities/finance/totalBalanceDTO";
 
 export interface IBalanceRepository {
-    getBalance(id_user: string): Promise<TotalBalanceDTO>
-    insertValueIntoTotalBalance(valueIntered: number, id_user: string): Promise<TotalBalanceDTO>
-    deductValueFromTheTotalBalance(amountToBeDeducted: number, id_user: string): Promise<TotalBalanceDTO>
+    getBalance(id_user: string): Promise<TotalBalanceDTO | NotFound>
+    insertValueIntoTotalBalance(valueIntered: number, id_user: string): Promise<TotalBalanceDTO | NotFound | NumberError>
+    deductValueFromTheTotalBalance(amountToBeDeducted: number, id_user: string): Promise<TotalBalanceDTO | NotFound | NumberError>
     amountSpentByTheUser(amountSpent: amountSpent, id_user: string): Promise<AmountSpentDTO>
 }
