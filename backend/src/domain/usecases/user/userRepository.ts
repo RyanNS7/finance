269e5375c35c3060d889278114ae650122e7b459
Ryanns7 <ryanns777@gmail.com>
2025-03-08 12:48:14 -0300

    refactor: added the error value inside the promise in case the function has any error

--- backend/src/domain/usecases/user/userRepository.ts
+++ backend/src/domain/usecases/user/userRepository.ts
@@ -1,9 +1,10 @@
+import { NotFound } from "../../../errors/baseError"
 import { User } from "../../entities/user/user"
 import { UserDTO } from "../../entities/user/userDTO"
 
 export interface IUserRepository{
     create(user: User): Promise<UserDTO>
     isValidEmail(email: string): Promise<boolean>
-    findUserById(id: string): Promise<UserDTO>
-    findUserByEmail(email: string): Promise<UserDTO>
+    findUserById(id: string): Promise<UserDTO | NotFound>
+    findUserByEmail(email: string): Promise<UserDTO | NotFound>
 }
