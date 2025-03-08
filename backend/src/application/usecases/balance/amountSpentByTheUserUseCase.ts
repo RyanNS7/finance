22c7add208cbede69105d9fc43531f653991a061
Ryanns7 <ryanns777@gmail.com>
2025-03-08 12:51:54 -0300

    perf: added a condition to check the results of the function

--- backend/src/application/usecases/balance/amountSpentByTheUserUseCase.ts
+++ backend/src/application/usecases/balance/amountSpentByTheUserUseCase.ts
@@ -1,5 +1,7 @@
 import { amountSpent } from "../../../domain/entities/finance/amountSpent";
+import { TotalBalanceDTO } from "../../../domain/entities/finance/totalBalanceDTO";
 import { IBalanceRepository } from "../../../domain/usecases/finance/balanceRepository";
+import { NotFound, NumberError } from "../../../errors/baseError";
 
 export class AmountSpentByTheUser{
     balanceRepo: IBalanceRepository
@@ -10,6 +12,22 @@ export class AmountSpentByTheUser{
 
     async amountSpent(amountSpent: amountSpent, id_user: string){
 
+        const balance = await this.balanceRepo.getBalance(id_user)
+        
+        if(balance instanceof NotFound){
+            return balance
+        }
+
+        if(amountSpent.money_spent > (balance as TotalBalanceDTO).totalBalance.total_balance){
+            return new NumberError("The number to withdraw is greater than the total balance")
+        }
+
+        const deduct = await this.balanceRepo.deductValueFromTheTotalBalance(amountSpent.money_spent, id_user)
+
+        if(deduct instanceof NotFound || deduct instanceof NumberError){
+            return deduct
+        }
+        
         const amountSpentByTheUser = await this.balanceRepo.amountSpentByTheUser(amountSpent, id_user)
 
         return amountSpentByTheUser
